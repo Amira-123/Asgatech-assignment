f515fcdcafd055e42b16b8a1348c23abcbc1a465
Amira Mohammed <70484629+Amira-123@users.noreply.github.com>
2023-07-14 17:30:06 +0200

    update-two

--- src/app/orders/services/order.service.ts
+++ src/app/orders/services/order.service.ts
@@ -1,15 +1,51 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
-import { IOrderVm } from '../utils/order.interface';
+import { BehaviorSubject, Observable, map, take, tap } from 'rxjs';
+import { Order } from '../utils/order.interface';
+import { ProductService } from 'src/app/products/services/product.service';
+import { Product } from 'src/app/products/utils/Products';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class OrderService {
+  constructor(
+    private http: HttpClient,
+    private productService: ProductService
+  ) {
+    this.getAllOrders().subscribe();
+  }
+  orderList: BehaviorSubject<Order[]> = new BehaviorSubject<Order[]>([]);
 
-  constructor(private http:HttpClient) { }
-  getAllOrders(){
-     return this.http.get<IOrderVm[]>(environment.baseApi+'orders.json')
+  orderList$: Observable<Order[]> = this.orderList.asObservable();
+
+  getOrderById(orderId: string): Order | undefined {
+    // TODO: Find the order by id
+    return this.orderList
+      .getValue()
+      .find((order: any) => order?.OrderId === orderId);
+  }
+
+  createOrder(order: Order): void {
+    // TODO: After collecting the order details
+    // TODO: Just update orderList
+  }
+
+  getAllOrders() {
+    return this.http.get<Order[]>(environment.baseApi + 'orders.json').pipe(
+      map((res) =>
+        res.map((order) => ({
+          ...order,
+          Products: order.Products?.map((prod) => ({
+            ...(this.productService.getProductById(prod.ProductId) as Product),
+            Quantity: prod.Quantity,
+          })),
+          // TODO: find and get the user by UserId
+        }))
+      ),
+      tap((res: Order[]) => this.orderList.next(res)),
+      take(1)
+    );
   }
 }
